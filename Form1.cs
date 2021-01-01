bfce6a9b080febb0a34d69d6bb838a0281738ed7
Busaruba2011 <65495333+Busaruba2011@users.noreply.github.com>
2021-01-01 05:55:25 -0800

    Add code

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+    using CefSharp.WinForms;
+
+namespace Sleepyheads
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeChromium();
+
+        }
+
+        public ChromiumWebBrowser chromeBrowser;
+
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+           
+            Cef.Initialize(settings);
+           
+            chromeBrowser = new ChromiumWebBrowser("https://busaruba2011.github.io/Sleepyheads/sounds.html");
+           
+            this.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+    }
+}
